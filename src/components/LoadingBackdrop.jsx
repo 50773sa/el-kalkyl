b6baeb464611c95001c408d68ede0dd201d013ac
50773sa <93046590+50773sa@users.noreply.github.com>
2022-12-26 14:34:07 +0100

    Fixed bug with redirect

--- src/components/LoadingBackdrop.jsx
+++ src/components/LoadingBackdrop.jsx
@@ -2,20 +2,18 @@ import Backdrop from '@mui/material/Backdrop';
 import CircularProgress from '@mui/material/CircularProgress';
 import { useState } from 'react';
 
-const LoadingBackdrop = ({ loading }) => {
+const LoadingBackdrop = () => {
     const [open, setOpen] = useState(true)
     
     return (
-        <>
-            {loading ? (
-                <Backdrop
-                    sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-                    open={open}
-                >
-                    <CircularProgress color="inherit" />
-                </Backdrop>
-            ): ''}
-        </>
+        <Backdrop
+            sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+            open={open}
+        >
+            <CircularProgress color="inherit" />
+        </Backdrop>
+           
+    
 
       
 
