22c2aa0d809e136edda2aafc79a526f12393d1bd
50773sa <93046590+50773sa@users.noreply.github.com>
2022-12-27 13:46:09 +0100

    Only show profile avatar if user is logged in

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,9 +6,34 @@ import Typography from '@mui/material/Typography'
 import Container from '@mui/material/Container'
 import Avatar from '@mui/material/Avatar'
 import Tooltip from '@mui/material/Tooltip'
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { useAuthContext } from '../contexts/AuthContextProvider';
+
+const settings = ['Profile', 'Logout'];
 
 
 const Navigation = () => {
+    const { currentUser }= useAuthContext
+
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+      };
+      const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+      };
+    
+      const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+      };
+    
+      const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+      };
+  
 
     return (
         <AppBar position="static">
@@ -40,13 +65,41 @@ const Navigation = () => {
                     {/**
                      *  avatar
                      */}    
+                     
+                    
+                    {currentUser ? (
+                        <>
+                            <Tooltip title="Go to my profile">
+                                <IconButton sx={{ p: 0 }}>
+                                    <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                                </IconButton>
+                            </Tooltip>
+                            <Menu
+                                sx={{ mt: '45px' }}
+                                id="menu-appbar"
+                                anchorEl={anchorElUser}
+                                anchorOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                open={Boolean(anchorElUser)}
+                                onClose={handleCloseUserMenu}
+                                >
+                                {settings.map((setting) => (
+                                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                                    <Typography textAlign="center">{setting}</Typography>
+                                    </MenuItem>
+                                ))}
+                            </Menu>
+                        </>
+                    ): ''}
 
-                    <Tooltip title="Go to my profile">
-                        <IconButton sx={{ p: 0 }}>
-                            <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-                        </IconButton>
-                    </Tooltip>
-
+                     
                 </Toolbar>
             </Container>
         </AppBar>
