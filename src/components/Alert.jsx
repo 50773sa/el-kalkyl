c1a720cd2f4902a553204105caf32db63f038120
50773sa <93046590+50773sa@users.noreply.github.com>
2022-12-28 15:08:53 +0100

    Added close button to modal

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -9,6 +9,7 @@ import DialogContent from '@mui/material/DialogContent'
 import DialogContentText from '@mui/material/DialogContentText'
 import DialogTitle from '@mui/material/DialogTitle'
 import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined'
+import CloseIcon from '@mui/icons-material/Close';
 
 
 const Alert = ({ open, setOpen }) => {
@@ -27,38 +28,46 @@ const Alert = ({ open, setOpen }) => {
                 aria-labelledby="alert-dialog-title"
                 aria-describedby="alert-dialog-description"
                 maxWidth='500px'
+      
             >
-                <div style={{ 
-                        display: 'flex', 
-                        justifyContent: 'center', 
-                        alignItems: 'center', 
-                        margin: '1rem', 
-                        backgroundColor: '#ffae0147', 
-                        width: '6rem', 
-                        height: '6rem', 
-                        borderRadius: '50%' 
-                    }}
-                >
+                <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
                     <div style={{ 
-                        display: 'flex', 
-                        justifyContent: 'center', 
-                        alignItems: 'center', 
-                        backgroundColor: '#ff590147', 
-                        width: '4rem', 
-                        height: '4rem', 
-                        borderRadius: '50%' 
-                    }}
-                >
-                        <SaveOutlinedIcon 
-                            fontSize='large' 
-                            style={{ color: '#ff5901', }}
-                        />
+                            display: 'flex', 
+                            flexDirection: 'row',
+                            justifyContent: 'center', 
+                            alignItems: 'center', 
+                            margin: '1rem', 
+                            backgroundColor: '#ffae0147', 
+                            width: '6rem', 
+                            height: '6rem', 
+                            borderRadius: '50%' 
+                        }}
+                    >
+                        <div style={{ 
+                            display: 'flex', 
+                            justifyContent: 'center', 
+                            alignItems: 'center', 
+                            backgroundColor: '#ff590147', 
+                            width: '4rem', 
+                            height: '4rem', 
+                            borderRadius: '50%' 
+                        }}
+                    >
+                            <SaveOutlinedIcon 
+                                fontSize='large' 
+                                style={{ color: '#ff5901', }}
+                            />
 
+                        </div>
+                    </div>
+
+                    <div style={{margin: '1rem'}} >
+                        <CloseIcon fontSize='large' onClick={handleClose}/>
                     </div>
                 </div>
-
+               
                 <DialogTitle id="alert-dialog-title">
-                    <strong>Du har inte sparat dina ändringar</strong>
+                    <strong>Du har inte sparat!</strong>
                 </DialogTitle>
 
                 <DialogContent>
