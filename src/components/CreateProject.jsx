122eb192d83a40d7be7665ecd14ac3ac3725bcca
50773sa <93046590+50773sa@users.noreply.github.com>
2023-01-06 20:24:40 +0100

    Now able to post array of products to firestore.

--- src/components/CreateProject.jsx
+++ src/components/CreateProject.jsx
@@ -1,74 +1,80 @@
-import React from 'react';
-import { useEffect, useRef, useState } from 'react'
+import React from 'react'
+import { useRef, useState } from 'react'
 import { db } from '../firebase'
-import { addDoc, collection  } from 'firebase/firestore';
-import { useAuthContext } from '../contexts/AuthContextProvider';
+import { uuidv4 } from '@firebase/util'
+import { addDoc, collection  } from 'firebase/firestore'
+import { useAuthContext } from '../contexts/AuthContextProvider'
 import { useForm } from 'react-hook-form';
-
+import LoadingBackdrop from './LoadingBackdrop'
+import useStreamCollection from '../hooks/useStreamCollection'
 import LeavePageAlert from './modals/LeavePageAlert'
-import useStreamCollection from '../hooks/useStreamCollection';
+import SuccessAlert from './modals/SuccessAlert'
 
 // mui
-import Box from '@mui/material/Box';
 import Button from '@mui/material/Button'
-import Checkbox from '@mui/material/Checkbox'
 import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined'
 import Grid from "@mui/material/Unstable_Grid2/Grid2"
 import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
-import MenuItem from '@mui/material/MenuItem'
-import Tab from '@mui/material/Tab';
-import TabContext from '@mui/lab/TabContext';
-import { TabList } from '@mui/lab';
-import TabPanel from '@mui/lab/TabPanel';
+import Tab from '@mui/material/Tab'
+import TabContext from '@mui/lab/TabContext'
+import { TabList } from '@mui/lab'
+import TabPanel from '@mui/lab/TabPanel'
 import TextField from '@mui/material/TextField'
 import Typography from '@mui/material/Typography'
-import { InputAdornment } from '@mui/material';
+import { InputAdornment } from '@mui/material'
 
 
 
 const CreateProject = () => {
-    const [loading, setLoading] = useState(true)
+    const [num, setNum] = useState(0)
     const [value, setValue] = useState('Apparater')
-    const [num, setNum] = useState()
-
+    const [selectedProduct, setSelectedProduct] = useState([])
     const [open, setOpen] = useState(false)
+    const [loading, setLoading] = useState(false)
     const [error, setError] = useState(null)
     const [success, setSuccess] = useState(false)
-    const [projectName, setProjectName] = useState()
-    const [checked, setChecked] = useState([0])
-    const [selectedProduct, setSelectedProduct] = useState([])
-    console.log('selectedProduct', selectedProduct)
-
-
     const { data: material, loading: isStreaming} = useStreamCollection('material', 'Apparater')
 
-    const projectRef = useRef()
     const { currentUser } = useAuthContext()
-    const { handleSubmit, formState: { errors }, reset, register, control } = useForm()
+    const { handleSubmit, formState: { errors }, reset, register } = useForm()
 
-
-    const handleChange = (event, newValue) => {
-      setValue(newValue);
+    const handleChange = (e, newValue) => {
+        e.preventDefault()
+        setValue(newValue);
     }
 
-    const handleDelete = (selectedItem) => () => {
-        setSelectedProduct((items) => items.filter((item) => item.id !== selectedItem.id));
-    };
+    const handleAdd = (item) => () => {
+        setLoading(true)
 
+        if (selectedProduct.includes(item)) {
+            return setLoading(false), 
+                console.log('Item already exists')
+        }
+        setSelectedProduct(selectedProduct => [...selectedProduct, item])
+        setLoading(false)
+    }
+
+
+    const handleDelete = (selectedItem) => () => {
+        setSelectedProduct((items) => items.filter((item) => item.id !== selectedItem.id))
+    }
+
+    
     const onSubmit = async (inputData) => {
         console.log('inputData', inputData)
         setError(null)
-        console.log('projectRef', projectRef)
-        setProjectName(projectRef.current.value)
-        console.log('projectName', projectName)
 
+        if (!inputData || selectedProduct) {
+            return
+        }
 
         try {
-            await addDoc(collection(db, 'project'), {
+            await addDoc(collection(db, 'projects'), {
                 uid: currentUser.uid,
-                id: projectRef.current.value
-        
+                id: uuidv4(),
+                projectName: inputData.projectName,
+                projectMaterial: selectedProduct
             })
             setSuccess(true)
             setOpen(true)
@@ -84,21 +90,16 @@ const CreateProject = () => {
     return (
         <div className='wrapper createProject' id='createProjectWrapper'>
 
-                <Typography
-                    variant="h6" 
-                    component="div" 
-                    textAlign='start' 
-                    marginBottom='2rem'
-                >
+            {loading ? <LoadingBackdrop /> : ''}
+
+            <Typography variant="h6" component="div" textAlign='start' marginBottom='2rem'>
                 <strong>Lägg till nytt projekt</strong> 
-                </Typography>
-                <form onSubmit={handleSubmit(onSubmit)} noValidate>
+            </Typography>
 
-
+            <form onSubmit={handleSubmit(onSubmit)} noValidate>
                 <Grid container spacing={2}>
-                    <Grid item xs={12} style={{ marginBottom: '6rem'}} >
+                    <Grid xs={12} style={{ marginBottom: '3rem'}} >
                         <TextField
-                            inputRef={projectRef}
                             required
                             fullWidth
                             id="projecttName"
@@ -110,8 +111,9 @@ const CreateProject = () => {
                                 required: true, 
                                 minLength: { value: 1, message: 'Obligatoriskt fält'}
                             })}
-                        />
+                        >
                         {errors.projectName === 'required' && <p>Obligatoriskt fält</p>}
+                        </TextField>
 
                     </Grid>
 
@@ -121,7 +123,7 @@ const CreateProject = () => {
 
                     <Grid xs={12}>
                         <TabContext value={value}>
-                            <Grid>
+                            <Grid xs={12}>
                                 <TabList onChange={handleChange} aria-label="tab list" style={{ marginBottom: '1rem' }}>
                                     <Tab className='tab' label="Apparater" value="Apparater" />
                                     <Tab className='tab' label="Belysning" value="Belysning" />
@@ -132,48 +134,45 @@ const CreateProject = () => {
                             <Grid xs={12} style={{ border: '1px solid #cacaca'}}>
                                 <TabPanel value="Apparater" style={{ height: '200px'}}>
                                     <List>
-                                        {material?.filter(list => list.category === "Apparater").map((item, i) => (
+                                        {!isStreaming ? material?.filter(list => list.category === "Apparater").map((item, i) => (
                                             <ListItem 
                                                 key={i} 
-                                                value={i.product}
-                                                onClick={() => (setSelectedProduct(selectedProduct => [...selectedProduct, item]))}
+                                                name="project"
+                                                onClick={handleAdd(item)}
                                                 disableGutters
-                                                {...register("product", {
-                                                    required: true, 
-                                                    minLength: { value: 1, message: 'Obligatoriskt fält'}
-                                                })}
-                                            > {item.product}
+                                            > 
+                                                {item.product}
                                             </ListItem>
-                                        ))}
+                                        )): ''}
                                     </List>
                                 </TabPanel> 
 
                                 <TabPanel value="Belysning" style={{ height: '200px'}}>
                                     <List>
-                                        {material?.filter(list => list.category === "Belysning").map((item, i) => (
+                                        {!isStreaming ? material?.filter(list => list.category === "Belysning").map((item, i) => (
                                             <ListItem 
                                                 key={i} 
-                                                value={i.product}
-                                                onClick={() => (setSelectedProduct(selectedProduct => [...selectedProduct, item]))}
+                                                onClick={handleAdd(item)}
                                                 disableGutters
-                                            > {item.product}
+                                            > 
+                                                {item.product}
                                             </ListItem>
-                                        ))}
+                                        )): ''}
                                     </List>
                                 </TabPanel>
 
                             
                                 <TabPanel value="Tele" style={{ height: '200px'}}>
                                     <List>
-                                        {material?.filter(list => list.category === "Tele").map((item, i) => (
+                                        {!isStreaming ? material?.filter(list => list.category === "Tele").map((item, i) => (
                                             <ListItem 
                                                 key={i} 
-                                                value={i.product}
-                                                onClick={() => (setSelectedProduct(selectedProduct => [...selectedProduct, item]))}
+                                                onClick={handleAdd(item)}
                                                 disableGutters
-                                            > {item.product}
+                                            > 
+                                                {item.product}
                                             </ListItem>
-                                        ))}
+                                        )): ''}
                                     </List>
                                 </TabPanel>
                             </Grid>
@@ -185,63 +184,66 @@ const CreateProject = () => {
                  *  Selected products
                  */}
 
-                <Grid container spacing={2} xs={12} alignItems='center'>
-                    <Grid item xs={12}>
-                        <List>
-                            {selectedProduct ? selectedProduct?.map((item, i) => (
-                                <ListItem
-                                    value={item}
-                                    key={item.id}
-                                    style={{ display: 'flex', justifyContent: 'space-between'}}
-                                > 
+                <Grid container spacing={2} style={{ marginBottom: "6rem" }}>
+                    {selectedProduct ? selectedProduct?.map((item, i) => (
+                        <>
+                            <Grid xs={6} display="flex" justifyContent="center" alignItems="center" >
+                                <ListItem value={item} key={item.id}> 
                                     {item.product}
-
-                                    <TextField
-                                        key={i}
-                                        type="number"
-                                        variant="outlined"
-                                        onChange={(e) => setNum(e.target.value)}
-                                        value={num}
-                                        size='small'
-                                        defaultValue='0'
-                                        style={{ width: '30%'}}
-                                        InputProps={{
-                                            inputMode: 'numeric', pattern: '[0-9]*',
-                                            endAdornment: <InputAdornment position="end">st</InputAdornment>,
-                                        }}
-                                    >
-                                    </TextField>
-                                    <DeleteOutlinedIcon 
-                                        onClick={handleDelete(item)}
-                                    />
                                 </ListItem>
-                            )): ''}
-                        </List>
-                    </Grid>
+                            </Grid>
+                        
+                            <Grid xs={4} display="flex" justifyContent="end" alignItems="center" >
+                                <TextField
+                                    key={i}
+                                    type="number"
+                                    variant="outlined"
+                                    onChange={(e) => setNum(Number(e.target.value))}
+                                    value={item.num}
+                                    defaultValue={0}
+                                    size='small'
+                                    InputProps={{
+                                        inputProps: {min: 0, max: 100 },
+                                        inputMode: 'numeric', pattern: '[0-9]*',
+                                        endAdornment: <InputAdornment position="end">st</InputAdornment>,
+                                    }}
+                                />
+                            </Grid>
+
+                            <Grid xs={2} display="flex" justifyContent="end" alignItems="center" color="red">
+                                <DeleteOutlinedIcon onClick={handleDelete(item)} />
+                            </Grid>
+
+                        </>
+                    
+                    )): ''}
                 </Grid>
 
-            {/**
-             *  Buttons
-             */}
+                {/**
+                 *  Buttons
+                 */}
 
-            <Grid className="buttonsWrap">
-                <Button 	
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    sx={{ mt: 3, mb: 2 }}
-                > Spara
-                </Button>
-                <Button
-                    fullWidth
-                    onClick={() => {setOpen( open ? false : true)}}
-                > Avbryt
-                </Button>
-            </Grid>
-        </form>
+                <Grid item xs={12} className="buttonsWrap">
+                    <Button 	
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                    > Spara
+                    </Button>
+                    <Button
+                        fullWidth
+                        onClick={() => {setOpen( open ? false : true)}}
+                    > Avbryt
+                    </Button>
+                </Grid>
+
+            </form>
 
 
             <LeavePageAlert open={open} setOpen={setOpen}/> 
+
+            {success ? <SuccessAlert open={open} setOpen={setOpen}/> : ''}
         
         </div>
     )
