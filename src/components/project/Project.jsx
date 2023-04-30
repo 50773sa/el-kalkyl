d4e2d21fe242e0de6d5ab43dbe53a7f69aa8a66d
50773sa <93046590+50773sa@users.noreply.github.com>
2023-04-30 13:15:58 +0200

    Delete and edit button

--- src/components/project/Project.jsx
+++ src/components/project/Project.jsx
@@ -8,10 +8,11 @@ import CalculationTable from './CalculationTable';
 import DialogDelete from '../modals/DialogDelete'
 import LoadingBackdrop from '../LoadingBackdrop'
 // mui
+import Button from '@mui/material/Button'
 import Typography from '@mui/material/Typography'
 import Grid from "@mui/material/Unstable_Grid2/Grid2";
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever'
-import ModeEditOutlineOutlinedIcon from '@mui/icons-material/ModeEditOutlineOutlined'
+import ModeEditIcon from '@mui/icons-material/ModeEdit'
 import ToggleOnIcon from '@mui/icons-material/ToggleOn'
 import ToggleOffIcon from '@mui/icons-material/ToggleOff'
 
@@ -48,8 +49,9 @@ const Project = ({ projectId }) => {
 			{loading && <LoadingBackdrop /> }
 
 			<Grid container spacing={2}>
-				<Grid 
-					xs={6} 
+				<Grid
+					xs={11} 
+					md={3}
 					display='flex' 
 					alignItems="center" 
 					justifyContent="space-between" 
@@ -63,38 +65,47 @@ const Project = ({ projectId }) => {
 				</Grid>
 
 				<Grid 
-					xs={2}
+					xs={1}
+					md={3}
 					display= 'flex'
-					justifyContent='end'
+					justifyContent={{ xs: 'end', md: 'start'}}
 					alignItems='center'
 				>
 					<span onClick={toggleProject}>
 						{project.completed 
-							? 	<ToggleOnIcon sx={{ color: '#15a715' }}/>  		
-							:	<ToggleOffIcon  sx={{ color: '#808080' }}/>
+							? 	<ToggleOnIcon sx={{ color: '#15a715' }}/>  	
+							:	<ToggleOffIcon  sx={{ color: '#808080' }}/>	
 						}
 					</span>
 				</Grid>
+				
+				<Grid
+					xs={12}
+					md={6}
+					sx={{ display: { xs: 'none ', md: 'flex'} }}
+					alignItems="center"
+					justifyContent="end"
+				>			
+					<Button 
+						sx={{ px: 1, mr: 2, width: '130px' }}
+						variant="outlined" 
+						onClick={() => navigate(`/user/${currentUser.uid}/project/${projectId}/edit`)} 				
+					>
+						<ModeEditIcon />
+						Redigera 
+					</Button>
 
-				<Grid 
-					xs={2}
-					display= 'flex'
-					justifyContent='end'
-					alignItems='center'
-				>
-					<ModeEditOutlineOutlinedIcon onClick={() => navigate(`/user/${currentUser.uid}/project/${projectId}/edit`)} />
-				</Grid>
-						
-				<Grid 
-					xs={2} 
-					display="flex" 
-					justifyContent="end" 
-					alignItems="center" 
-					color="red" 
-				>
-					<DeleteForeverIcon onClick={deleteProject} />
-				</Grid>
+					<Button 
+						sx={{ px: 1, width: '130px' }}
+						variant="outlined" 					 
+						onClick={deleteProject} 				
+					>
+						<DeleteForeverIcon  />
+						Radera 
+					</Button>
+			
 
+				</Grid>
 				
 				{/**
 				 * 	Table
@@ -102,6 +113,34 @@ const Project = ({ projectId }) => {
 
 				<Grid item xs={12}>
 					<CalculationTable project={project} projectId={projectId}/>
+				</Grid>
+
+				<Grid
+					xs={12}
+					md={6}
+					sx={{ display: { md: 'none '} }}
+					alignItems="center"
+					justifyContent="end"
+				>			
+					<Button 
+						sx={{ px: 1, mr: 2, width: '130px' }}
+						size='large'
+						variant="outlined" 
+						onClick={() => navigate(`/user/${currentUser.uid}/project/${projectId}/edit`)} 				
+					>
+						<ModeEditIcon />
+						Redigera 
+					</Button>
+
+					<Button 
+						sx={{ px: 1, width: '130px' }}
+						size='large'
+						variant="outlined" 					 
+						onClick={deleteProject} 				
+					>
+						<DeleteForeverIcon  />
+						Radera 
+					</Button>
 				</Grid>
 			</Grid>
 
