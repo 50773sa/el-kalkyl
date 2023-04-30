7475f61919e4219e206e2feec867c187f028aa11
50773sa <93046590+50773sa@users.noreply.github.com>
2023-04-30 17:35:22 +0200

    Bugfix when calculating estimated time

--- src/components/helpers/calculateWorkHours.js
+++ src/components/helpers/calculateWorkHours.js
@@ -1,37 +1,20 @@
 import { useEffect, useState } from "react"
 
 export default function calculateWorkHours(projects) {
-    const [loading, setLoading] = useState(true)
-    const [hours, setHours] = useState(null)
-    const [minutes, setMinutes] = useState(null)
-
-    const calculate = () => {
-
-         projects?.projectMaterial.map(p => {
-            console.log('p?*************', p?.estimatedTime)
-            let workingMinutes = (p?.estimatedTime?.hours * 60) + p?.estimatedTime?.minutes
-            let totalMinutes = workingMinutes * p?.quantity
-            console.log('totalMinutes', totalMinutes)
-
-            console.log('workingMinutes', workingMinutes)
-            setHours(Math.floor(totalMinutes / 60))
-            setMinutes(totalMinutes % 60)
-            setLoading(false)
-
-        })
-    }
+    const [hours, setHours] = useState(0)
+    const [minutes, setMinutes] = useState(0)
     
     useEffect(() => {
-        setLoading(true)
 
-        // console.log('projects', projects)
-        if (hours === null) {
-            return
-        }
-        calculate(projects)
-        console.log('projects', projects)
-        
-   
+        projects && projects?.projectMaterial.map(p => {
+
+            let workingMinutes = (p?.estimatedTime?.hours * 60) + p?.estimatedTime?.minutes
+            let totalMinutes = workingMinutes * p?.quantity
+
+            setHours(Math.floor(totalMinutes / 60))
+            setMinutes(totalMinutes % 60)
+        })  
+
        
     }, [projects])
 
