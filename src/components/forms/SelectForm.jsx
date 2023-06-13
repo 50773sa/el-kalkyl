84cd5bc2cfadab09224d8ea4fd5958cd8f0c5440
50773sa <93046590+50773sa@users.noreply.github.com>
2023-06-13 10:38:02 +0200

    Create reusable select-form

---
+++ src/components/forms/SelectForm.jsx
@@ -0,0 +1,30 @@
+import MenuItem from '@mui/material/MenuItem'
+import TextField from '@mui/material/TextField'
+
+
+const SelectForm = ({ required, label, name, inputRef, defaultValue, list, helperText, register }) => {
+    return (
+        <TextField
+            select
+            required={required}
+            label={label}
+            name={name}
+            inputRef={inputRef}
+            defaultValue={defaultValue}
+            fullWidth
+            helperText={helperText}
+            {...register}
+        >
+                
+            {list.map((option, i) => {
+                return (
+                    <MenuItem key={option.value} value={option.value}>
+                        {option.value}
+                    </MenuItem>
+                )
+            })}
+        </TextField>
+    )
+}
+
+export default SelectForm
