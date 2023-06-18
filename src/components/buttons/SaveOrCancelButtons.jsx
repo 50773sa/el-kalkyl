7c895e69a49731ec540adf8b5d2d04655fcb739b
50773sa <93046590+50773sa@users.noreply.github.com>
2023-06-18 19:58:40 +0200

    Make the buttons reusable

--- src/components/buttons/SaveOrCancelButtons.jsx
+++ src/components/buttons/SaveOrCancelButtons.jsx
@@ -2,27 +2,27 @@
 import Button from '@mui/material/Button'
 import Grid from "@mui/material/Unstable_Grid2/Grid2"
 
-const SaveOrCancelButtons = ({ setOpen }) => {
+const SaveOrCancelButtons = ({ setOpen, success }) => {
     return (
-        <Grid container>
+        <Grid xs={12} display="flex" justifyContent="center" flexDirection="column" alignItems="start" className="buttons">
             <Grid xs={12} md={3}>
                 <Button 	
                     type="submit"
                     fullWidth
                     variant="contained"
-                    sx={{ mt: 3, mb: 2 }}
+                    sx={{ mt: 3, mb: 2, p: 1 }}
                 > Spara
                 </Button>
-            </Grid>
-            <Grid xs={12} md={2} display="flex" alignItems="center">
                 <Button
                     fullWidth
                     disableRipple
-                    onClick={() => {setOpen( open ? false : true)}}
-                > Avbryt
+                    onClick={() => {!success ? setOpen(true) : ''}}
+                    > Avbryt
                 </Button>
             </Grid>
+
         </Grid>
+
     )
 }
 
