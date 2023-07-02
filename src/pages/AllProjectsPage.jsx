09ec8f5f6eb3b141ef60db17cd408ce0b249948c
50773sa <93046590+50773sa@users.noreply.github.com>
2023-07-02 10:43:15 +0200

    Get id from url

--- src/pages/AllProjectsPage.jsx
+++ src/pages/AllProjectsPage.jsx
@@ -7,12 +7,15 @@ import useViewStore from '../store/useViewStore'
 // hooks
 import useGetAuthColl from '../hooks/useGetAuthColl'
 import useStreamCollection from "../hooks/useStreamCollection"
-
+import { useParams } from 'react-router-dom'
 
 const AllProjectsPage = () => {
+    const { id } = useParams()
     const { data: projects, isLoading, isError } = useGetAuthColl('projects')
     const { data: material, loading} = useStreamCollection('material')
 	const isCurrentView = useViewStore((state) => state.isCurrentView)
+
+    console.log('projects', id)
 
     return (
         <ProjectAndMaterialPageWrapper tabTitle1="Projekt" tabTitle2="Nytt projekt">
@@ -23,7 +26,7 @@ const AllProjectsPage = () => {
             {projects && !loading && material && (
                 isCurrentView.collection 
                     ?   <AllProjects projects={projects} />
-                    :   <CreateProject material={material} />                               
+                    :   <CreateProject material={material} currentUser={id} projects={projects} />                               
             )}
 
         </ProjectAndMaterialPageWrapper>                
