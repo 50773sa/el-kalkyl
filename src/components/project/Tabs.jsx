7708f987f2b72a69ea2a98a477bcbf58d34f48f1
50773sa <93046590+50773sa@users.noreply.github.com>
2023-07-06 14:58:37 +0200

    Remove grid

--- src/components/project/Tabs.jsx
+++ src/components/project/Tabs.jsx
@@ -4,14 +4,12 @@ import TabList  from '@mui/lab/TabList'
 
 const Tabs = ({ handleChange }) => {
     return (
-        <Grid xs={12} padding={0}>
-            <TabList onChange={handleChange} aria-label="tab list" style={{ display: 'flex', overflowX: 'scroll' }}>
-                <Tab className='tab' label="Apparater" value="Apparater" />
-                <Tab className='tab' label="Belysning" value="Belysning" />
-                <Tab className='tab' label="Data" value="Data" />
-                <Tab className='tab' label="Övrigt" value="Ovrigt" />
-            </TabList>
-        </Grid>
+        <TabList onChange={handleChange} aria-label="tab list" sx={{ display: 'flex', overflow: 'auto' }}>
+            <Tab className='tab' label="Apparater" value="Apparater" />
+            <Tab className='tab' label="Belysning" value="Belysning" />
+            <Tab className='tab' label="Data" value="Data" />
+            <Tab className='tab' label="Övrigt" value="Ovrigt" />
+        </TabList>
     )
 }
 
