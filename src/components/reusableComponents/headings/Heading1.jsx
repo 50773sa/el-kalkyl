0e14119973e0db529a128bd4450dbfc72ebb4c61
50773sa <93046590+50773sa@users.noreply.github.com>
2023-08-02 17:23:22 +0200

    Move/add toggle-btn and edit-btn to heading

--- src/components/reusableComponents/headings/Heading1.jsx
+++ src/components/reusableComponents/headings/Heading1.jsx
@@ -1,28 +1,42 @@
+import EditButton from "../../buttons/EditButton"
 // mui
+import { useTheme } from "@mui/material"
 import Grid from "@mui/material/Unstable_Grid2/Grid2"
 import Typography from '@mui/material/Typography'
 
-const Heading1 = ({ h1 }) => {
+const Heading1 = ({ h1, icon, isEditBtn, onclick, buttonText }) => {
+
     return (
-        <Grid xs={12} sx={{ height:'50px', backgroundColor: '#e0e0e0', marginBottom: 4 }}>
-            <Typography 
-                component="div" 
-                variant="h1" 
-                sx={{ 
-                    display: 'flex', 
-                    alignItems: 'center', 
-                    height: '100%', 
-                    px: 2,
-                    color: '#606060', 
-                    fontSize: '14px', 
-                    textTransform: 'upperCase',
-                    fontWeight: 600,
-                    cursor: "default" 
-                }}
-            >
-                {h1}
-            </Typography>    
-        </Grid>  
+        <Grid container sx={{ justifyContent: 'space-between', height:'50px', backgroundColor: '#e0e0e0', marginBottom: 2 }}>
+            <Grid xs={7} sm={8} md={9}>
+                <Typography 
+                    component="div" 
+                    variant="h1" 
+                    sx={{ 
+                        display: 'flex', 
+                        alignItems: 'center', 
+                        height: '100%', 
+                        px: 2,
+                        color: '#606060', 
+                        fontSize: '14px', 
+                        textTransform: 'upperCase',
+                        fontWeight: 600,
+                        cursor: "default" 
+                    }}
+                >
+                    {h1} 
+                </Typography>  
+            </Grid>
+
+            <Grid xs={5} sm={4} md={2} sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', pr: 2 }}>
+                {icon}
+
+                {isEditBtn && (
+                    <EditButton onClick={onclick} buttonText={buttonText} />
+                )}
+            </Grid>  
+        </Grid>
+
     )
 }
 
